2fc54bcef3a408f6f37cbd174e7c490b654ddc22
SUZANE ARCANJO DA HORA <23111828@CFP127ALUNO.LOCAL>
2024-03-28 16:54:45 -0300

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- prisma/migrations/20240208145929_backup08022024/migration.sql
+++ prisma/migrations/20240208145929_backup08022024/migration.sql
@@ -1,7 +1,6 @@
 -- CreateTable
-CREATE TABLE `tbl_filme` (
+CREATE TABLE `tbl_test` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(191) NULL,
-
+    `nome` VARCHAR(191) NOT NULL,
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
